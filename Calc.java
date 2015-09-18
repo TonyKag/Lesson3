f7759ea3f0b731eaba261f4309b65d1b43e51aa8
TonyKag <tonykag@mail.com>
2015-09-17 18:06:29 -0700

    class Calc created

---
+++ Calc.java
@@ -0,0 +1,21 @@
+// 1-st operand: three arguments A, S, M, D, and MM
+// 2-nd arg: number
+// 3-d arg: number
+
+public class Calc {
+	public static void main(String[] args) {
+		String operation = "Q"
+		String a = "A";
+		String s = "S";
+		String m = "D";
+		String mm = "MM";
+		int first = 0;
+		int second = 0;
+
+		for (int i = 0; i < args.length ) {
+			switch (operation) {
+				case "A":
+		}
+		}
+	}
+}
